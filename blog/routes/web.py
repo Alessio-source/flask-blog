7a0389ed1a492f89823eac215eca76b811dc7785
Alessio <unconfigured@null.spigotmc.org>
2022-04-08 18:33:20 +0200

    update articoli

--- blog/routes/web.py
+++ blog/routes/web.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, flash, url_for, session
+from flask import render_template, redirect, flash, url_for, session, request, abort
 from flask_login import current_user, login_user, logout_user
 from blog import app, db
 from blog.forms import LoginForm, PostForm, RegisterForm
@@ -81,10 +81,33 @@ def post_create():
                 db.session.add(new_post)
                 db.session.commit()
                 return redirect(url_for('posts'))
-            else:
-                return render_template("/admin/post.html", current_user=current_user, form=form)
+
+            return render_template("/admin/post.html", current_user=current_user, form=form)
 
         return redirect(url_for('homepage'))
+
+@app.route('/admin/post/<int:post_id>/edit', methods=["POST", "GET"])
+def post_edit(post_id):
+    if current_user.is_authenticated:
+        if current_user.role_id == 2 or current_user.role_id == 3:
+            post = Post.query.get_or_404(post_id)
+            if post.author_id == current_user.id or current_user.role_id == 2:
+                form = PostForm()
+                if form.validate_on_submit():
+                    post.title = form.title.data
+                    post.subtitle = form.subtitle.data
+                    post.content = form.content.data
+                    db.session.commit()
+                    return redirect(url_for('posts'))
+
+                form.title.data = post.title
+                form.subtitle.data = post.subtitle
+                form.content.data = post.content
+                return render_template("/admin/post.html", current_user=current_user, form=form)
+            else: 
+                abort(403)
+
+    return redirect(url_for('homepage'))
 
 @app.route("/admin/users")
 def users():
